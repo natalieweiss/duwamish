updated fixing ids and removed incorrect unit conversion of SLs

code/join_to_sample_points.py
@@ -3,66 +3,54 @@ import geopandas as gp
 import numpy as np
 from datetime import date
 
-def main(sample_outing_name, processed_path, sample_pts_path):
-    # create output file names
+def main(sample_outing_name, processed_path, sample_pts_path, qaqc_path):
+
+    """ 
+    This function takes in the following inputs:
+    sample_outing_name, str: month and year of the raw data processed
+    qaqc_path, str: the path of the QAQC folder
+    sample_pts_path, str: path of the Master Sampling Sites spreadsheet to check for matching sample IDs
+    processed_path, str: path for output file 
+    
+    The output is a single file with processed data join screening levels and sample point geometries
+    to be mapped and entered in Tableau
+    """
+
+    # Create output file names
     input_results_path = f"{processed_path}/{sample_outing_name}_results_joined_SL.csv"
     output_results_path = f"{processed_path}/{sample_outing_name}_results_join_geometry.csv"
 
+    # Read in sample points
     sample_pts = pd.read_csv(sample_pts_path)
     sample_pts_gdf = gp.GeoDataFrame(sample_pts, geometry=gp.points_from_xy(sample_pts.Longitude, sample_pts.Latitude), crs="EPSG:4326")
 
-    ## TODO: replace all fixed IDs in the processing raw_f_and_b.py script as the first step
-    ## TODO: replace all dates to match the date in the master sampling spreadsheet
-    # replace SI-W-2 to S1-W-2 to match results spreadsheet
-    sample_pts_gdf['Sampling ID_match_fb'] = np.where(sample_pts_gdf['Sampling ID'] == 'SI-W-2', 'S1-W-2', sample_pts_gdf['Sampling ID'])
-
-    # replace BI-W-1 to B1-W-1 to match results spreadsheet
-    sample_pts_gdf['Sampling ID_match_fb'] = np.where(sample_pts_gdf['Sampling ID_match_fb'] == 'BI-W-1', 'B1-W-1', sample_pts_gdf['Sampling ID_match_fb'])
-
-    # replace DP-S-1 to DPS-1 to match results spreadsheet
-    sample_pts_gdf['Sampling ID_match_fb'] = np.where(sample_pts_gdf['Sampling ID_match_fb'] == 'DP-S-1', 'DPS-1', sample_pts_gdf['Sampling ID_match_fb'])
-
-    # replace BI-55-W to BI-55-1-W to match results spreadsheet
-    sample_pts_gdf['Sampling ID_match_fb'] = np.where(sample_pts_gdf['Sampling ID_match_fb'] == 'BI-55-W', 'BI-55-1-W', sample_pts_gdf['Sampling ID_match_fb'])
-
-    # replace date for BIS-1-2 from 11/9/2023 to 11/8/2023
-    sample_pts_gdf['Date_match_fb'] = np.where(sample_pts_gdf['Sampling ID_match_fb'] == 'BIS-1-2', date(2023, 11, 8).strftime('%m/%d/%Y'), sample_pts_gdf['Date'])
-    sample_pts_gdf['Date_match_fb'] = pd.to_datetime(sample_pts_gdf['Date_match_fb'])
-
-    # read in processed results
+    # Read in processed results
     screening_results = pd.read_csv(input_results_path)
     cols = screening_results.columns.to_list()
     cols += ['Latitude', 'Longitude', 'Description', 'MILE']
 
-    # strip all sample ids of leading spaces
+    # Strip all sample ids of leading spaces
     screening_results['Sample ID'] = screening_results['Sample ID'].str.strip()
-
-    # replace sampling ID for SPB-0159-S-1 to SPB-O159-S-1
-    screening_results['Sample ID'] = np.where(screening_results['Sample ID'] == 'SPB-0159-S-1', 'SPB-O159-S-1', screening_results['Sample ID'])
-
-    # TODO: remove any screening results with U and C qualifiers. do not want to show these in the map
 
     try:
         screening_results['DATE'] = screening_results['DATE'].str[:10]
         screening_results["DATE"] = pd.to_datetime(screening_results["DATE"])
     except:
         print("date not included")
-
-
-    # replace date for BIS-1-2 from 11/9/2023 to 11/8/2023
-    screening_results['DATE'] = np.where(screening_results['Sample ID'] == 'BIS-1-2', np.datetime64(date(2023, 11, 8)), screening_results['DATE'])
+    
+    # Join sample points geometry to the results based on sample ID
     sample_pts_gdf["Date"] = pd.to_datetime(sample_pts_gdf["Date"], format='mixed')
-
-    sample_pts_join_results = pd.merge(screening_results, sample_pts_gdf, left_on = ['Sample ID', 'DATE'], right_on = ['Sampling ID_match_fb', 'Date'], how = 'left')
+    sample_pts_join_results = pd.merge(screening_results, sample_pts_gdf, left_on = ['Sample ID', 'DATE'], right_on = ['Sampling ID', 'Date'], how = 'left')
     sample_pts_join_results.rename(columns = {'Medium_x':'Medium'}, inplace = True)
 
     sample_pts_join_results_gdf = gp.GeoDataFrame(sample_pts_join_results)
     sample_pts_join_results_gdf['Date'] = sample_pts_join_results_gdf['Date'].astype(str)
 
+    # Export results
     sample_pts_join_results[cols].to_csv(output_results_path, index = False)
 
-
+    # Export IDs with missing geometries to a table
     missing_ids = sample_pts_join_results[sample_pts_join_results['Latitude'].isna()]
     if len(missing_ids)>0:
-        missing_ids[['DATE', 'Sample ID', 'Sampling ID', 'Medium', 'Latitude', 'Longitude','Description']].to_csv(f"/home/nweiss/gdrive/Year 2/Summer - Duwamish/Sampling_Results/qaqc/{sample_outing_name}_missing_pts.csv", index = False)
+        missing_ids[['DATE', 'Sample ID', 'Sampling ID', 'Medium', 'Latitude', 'Longitude','Description']].to_csv(f"{qaqc_path}/{sample_outing_name}_missing_pts.csv", index = False)
 

code/join_to_screening_levels.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import numpy as np
-from datetime import date
-import os
 
 
 def drop_levels(df):
+
+    """ This function flattens a multindex dataframe to a single dataframe"""
     df.reset_index(inplace = True)
     df.columns = df.columns.droplevel(1)
     return df
 
-## This script joins the combined F&B results to the screening levels
-## The only input is the scenario name as an input
-
-# for qaqc:
-# a list of all chemicals with screening levels
-# a list of all chemicals with tests that have been run
-
 def main(sample_outing_name, processed_path, qaqc_path, sl_path, pcb_arc_lookup_path):
+
+    """ 
+    This function takes in the following inputs:
+    sample_outing_name, str: month and year of the raw data processed
+    qaqc_path, str: the path of the QAQC folder
+    sample_pts_path, str: path of the Master Sampling Sites spreadsheet to check for matching sample IDs
+    processed_path, str: path for output file 
+    sl_path, str: path for Master screening level spreadsheet
+    pcb_arc_lookup_path: path for lookup table cross referencing PCB congener formulas with PCB numbers and aroclor
+    
+    The output is a single file with processed data join screening levels and the screening level exceedance
+    """
 
     # create file paths
     if sample_outing_name == 'prev_results':
@@ -28,20 +33,18 @@ def main(sample_outing_name, processed_path, qaqc_path, sl_path, pcb_arc_lookup_
     
     results_df = pd.read_csv(input_results_path)
 
-    #convert units from pg/L -> ug/L
+    # Convert units of the results
+    # convert units from pg/L -> ug/L
     results_df['Result Value'] = np.where(results_df['Result Value Units']=='pg/L', results_df['Result Value']/1000000, results_df['Result Value'])
     results_df['Result Value Units'] = np.where(results_df['Result Value Units']=='pg/L', 'ug/L', results_df['Result Value Units'])
 
-    #convert units from mg/L -> ug/L
+    # convert units from mg/L -> ug/L
     results_df['Result Value'] = np.where(results_df['Result Value Units']=='mg/L', results_df['Result Value']/1000, results_df['Result Value'])
     results_df['Result Value Units'] = np.where(results_df['Result Value Units']=='mg/L', 'ug/L', results_df['Result Value Units'])
 
-    #convert units pg/g -> mg/kg
+    # convert units pg/g -> mg/kg
     results_df['Result Value'] = np.where(results_df['Result Value Units']=='pg/g', results_df['Result Value']/1000000, results_df['Result Value'])
     results_df['Result Value Units'] = np.where(results_df['Result Value Units']=='pg/g', 'mg/kg', results_df['Result Value Units'])
-
-
-    # JOIN TABLES OF RESULTS TO MASTER SCREENING LEVELS FROM F&B
 
     # create data frame of the screening levels for soils and
     sl_soil_df = pd.read_excel(sl_path, sheet_name='Soil')
@@ -55,9 +58,11 @@ def main(sample_outing_name, processed_path, qaqc_path, sl_path, pcb_arc_lookup_
     sl = sl[(sl['Screening Level']!='TBD')]
     sl = sl[(sl['Screening Level']!='PQL')]
     sl = sl[(sl['Screening Level'].isna()==False)]
+
+    # Convert accidental PCBs screening levels to 0
     sl['Screening Level'] = np.where(sl['Screening Level'] == 'Present', 0, sl['Screening Level'])
 
-
+    # Convert units of the screening leves
     sl['Screening Level'] = sl['Screening Level'].astype(float)
     sl['Screening Level'] = np.where((sl['SL Unit'] =='mg/L')  & (sl['Medium'] =='Water'), sl['Screening Level']/1000, sl['Screening Level'])
     sl['SL Unit'] = np.where(sl['SL Unit']=='mg/L', 'ug/L', sl['SL Unit'])
@@ -71,21 +76,19 @@ def main(sample_outing_name, processed_path, qaqc_path, sl_path, pcb_arc_lookup_
     sl['Screening Level'] = np.where((sl['SL Unit'] =='ppb') & (sl['Medium'] =='Soil'), sl['Screening Level']*0.001, sl['Screening Level'])
     sl['SL Unit'] = np.where((sl['SL Unit'] =='ppb') & (sl['Medium'] =='Soil'), 'mg/kg', sl['SL Unit'])
 
-
-    print(sl['Screening Level'].dtype)
-    
+    # Export values for QAQC
     sl_qaqc = sl[['Medium', 'Chemical Group','Chemical', 'Scenario','Scenario Detail','Pathway', 'Screening Level']]
     sl_qaqc.drop_duplicates(inplace = True)
     sl_qaqc.to_csv(f"{qaqc_path}/screening_levels_identified.csv", index = False)
 
-    # strip dioxin furans screening levels of their commas to match the results spreadsheet
+    # Strip dioxin furans screening levels of their commas to match the results spreadsheet
     sl['Chemical'] = np.where(sl['Chemical Group']== 'Dioxin Furans', sl['Chemical'].str.replace(',',''), sl['Chemical'])
 
-    # strip pcbs of their commas to match the results spreadsheet
+    # Strip pcbs of their commas to match the results spreadsheet
     sl['Chemical_fmt'] = np.where(sl['Chemical Group']== 'PCB', sl['Chemical'].str.replace(',',''), sl['Chemical'])
 
 
-    # JOIN SCREENING LEVELS TO RESULTS
+    #### JOIN SCREENING LEVELS TO RESULTS ###
 
     # create data frames of the raw data and the lookup
     pcb_arc_lookup = pd.read_csv(pcb_arc_lookup_path)
@@ -111,12 +114,11 @@ def main(sample_outing_name, processed_path, qaqc_path, sl_path, pcb_arc_lookup_
     sl_results_join.dropna(subset=['Sample ID'], inplace=True)
     sl_results_join.rename(columns = {'Field Collection Start Date': 'DATE'}, inplace = True)
 
-
     sl_results_join['Medium'] = np.where(sl_results_join['Medium'].isna() == True, sl_results_join['Sample Matrix_clean'], sl_results_join['Medium'])
     sl_results_join['Chemical_fmt'] = np.where(sl_results_join['Chemical_fmt'].isna() == True, sl_results_join['Result Parameter Name'], sl_results_join['Chemical_fmt'])
 
-
     # CALCUALTE MOST STRINGENT AND COUNT OF STRINGENT EXCEEDED
+    """
     sl_arc_join = sl_arc_join[sl_arc_join['Screening Level']!='N/A']
     sl_arc_join = sl_arc_join[(sl_arc_join['Screening Level']!='na')]
     sl_arc_join = sl_arc_join[(sl_arc_join['Screening Level']!='TBD')]
@@ -131,23 +133,23 @@ def main(sample_outing_name, processed_path, qaqc_path, sl_path, pcb_arc_lookup_
     #For the results that signify most stringent, add column indicating stringent value for filtering
     sl_results_join = sl_results_join.merge(sl_stringent, how = 'left', indicator = True, on = ['Medium', 'Chemical Group', 'Chemical_fmt', 'Scenario'])
     sl_results_join['stringent_ind'] = np.where(sl_results_join['_merge']=='both', 'Stringent','')
-
+    
+    
 
     sl_results_join.drop(columns = 'Screening Level_y', inplace = True)
     sl_results_join.rename(columns = {'Screening Level_x':'Screening Level'}, inplace = True)
+    """
 
     sl_results_join['Chemical'] = np.where(sl_results_join['Screening Level']=='No Screening Level Identified', sl_results_join['Result Parameter Name'], sl_results_join['Chemical'])
 
     try:
         columns = ['DATE','Sample ID','Medium', 'Chemical Group', 'Chemical', 'Land Use', 'Target Receptor', 'Transport Pathway', 'Exposure Pathway', 'Scenario', 'Pathway_for_Report', 'Screening Level', 'SL Unit',
-            'Source', 'Source Number','Reference', 'Result Value','Result Value Units','Result Data Qualifier', 'SL_exceeded', 'SL_diff','stringent_ind']
+            'Source', 'Source Number','Reference', 'Result Value','Result Value Units','Result Data Qualifier', 'SL_exceeded', 'SL_diff']
         sl_results_join[columns].to_csv(f'{processed_path}/{output_results_path}.csv', index = False)
     except:
         columns = ['DATE','Sample ID','Medium', 'Chemical Group', 'Chemical', 'Land Use', 'Target Receptor', 'Transport Pathway', 'Exposure Pathway', 'Scenario', 'Pathway_for_Report', 'Screening Level', 'SL Unit',
-        'Source', 'Source Number','Reference', 'Result Value','Result Value Units','SL_exceeded','SL_diff','stringent_ind']
+        'Source', 'Source Number','Reference', 'Result Value','Result Value Units','SL_exceeded','SL_diff']
         sl_results_join[columns].to_csv(f'{processed_path}/{output_results_path}.csv', index = False)
-
-    #sl_results_join[columns].to_csv(f'{processed_path}/{output_results_path}.csv', index = False)
 
     qaqc_df = sl_results_join[sl_results_join['Screening Level'] != 'No Screening Level Identified']
     qaqc_df= qaqc_df[['DATE','Sample ID', 'Medium', 'Chemical Group','Chemical','Result Value']]

code/process_raw_f_and_b_data.py
@@ -7,30 +7,46 @@ from datetime import date
 
 
 ## This script takes a path to a folder containing results from F&B and the name of the sampling outing 
-## Note: All results must be in .xlsx format in order to be read by the script
+
 def drop_levels(df):
+    """ This function flattens a multiindex dataframe once aggregated"""
     df.reset_index(inplace = True)
     df.columns = df.columns.droplevel(1)
     return df
 
 def clean_pcb(val):
+    """ This function removes the / from specific PCB congeners to allow for accurate joins"""
     if 'aroclor' in val:
         val = val
     elif 'PCB' in val:
         val = val.replace("/"," ").split(" ")[1]
     return val
 
-def main(sample_outing_name, qaqc_path, sample_pts_path, raw_data_path, processed_path):
+def main(sample_outing_name, qaqc_path, sample_pts_path, fixed_id_path, raw_data_path, processed_path):
+
+    """ 
+    This function takes in the following inputs:
+    sample_outing_name, str: month and year of the raw data processed
+    qaqc_path, str: the path of the QAQC folder
+    sample_pts_path, str: path of the Master Sampling Sites spreadsheet to check for matching sample IDs
+    raw_data_path, str: path of the raw .xls file
+    processed_path, str: path for output file 
+    
+    The output is a single file with processed raw data with cleaned IDs to match the master sampling sites
+    sheet and chemical names to match the screening level spreadsheet
+    """
+
     print(f"Processing:{raw_data_path}")
 
-    # folder containing spreadsheets from F & B
+    # Initiates file paths for output qaqc and processed files
     processed_path = f"{processed_path}/{sample_outing_name}_results.csv"
     qaqc_file = f"{qaqc_path}/{sample_outing_name}_qaqc.csv"
 
-    # PROCESS RESULTS SPREADSHEETS
+    # Initiates file extension of the raw data files from F&B.
+    # Currently provided in .xls file
     file_extension = '*.xls'
 
-    # List all files with the specified extension in the folder
+    # List all files with the specified extension in the raw data folder
     files = glob.glob(os.path.join(raw_data_path, file_extension))
 
     results_df = []
@@ -42,11 +58,12 @@ def main(sample_outing_name, qaqc_path, sample_pts_path, raw_data_path, processe
     # Iterate through each file and read its content
     for file in files:
         print(f"Processing: {file}")
-        df = pd.read_excel(file, sheet_name = 'Sheet1')
-        df.columns = df.columns.str.replace("_"," ")
-        df['Sample ID'] = df['Sample ID'].str.strip()
-        results_df.append(df)
+        df = pd.read_excel(file, sheet_name = 'Sheet1') # read into a dataframe
+        df.columns = df.columns.str.replace("_"," ") # replace underscores in columns to spaces
+        df['Sample ID'] = df['Sample ID'].str.strip() # strip the sample IDs of any stray spaces
+        results_df.append(df) # append into the results data frame
 
+        # write out metadata for qaqc
         try:
             # write out file paths, length of data frame, and sample IDs for qa / qc
             sample_ids.append(list(df['Sample ID'].unique()))
@@ -56,20 +73,20 @@ def main(sample_outing_name, qaqc_path, sample_pts_path, raw_data_path, processe
         except:
             continue
 
+    # Concatenate all of the results into one data frame
     results_df = pd.concat(results_df)
     results_df['Sample ID'] = results_df['Sample ID'].str.strip()
 
-    #### QAQC check to make sure that the sample ids match what is in our sampling sites spreadsheet
-    sample_pts_gdf = gp.read_file(sample_pts_path)
+    # Check against the Master Sampling Sites spreadsheet to see if the IDs match F&B
+    # If there is a match that is incorrect, add the ID to the Fixed IDs sheet
+    sample_pts_gdf = gp.read_file(sample_pts_path) # sampl
     sample_pt_ids = sample_pts_gdf['Sampling ID'].unique()
     no_match = []
     for f_b_ids in sample_ids:
         for f_b_id in f_b_ids:
             if f_b_id in sample_pt_ids: # check to see if f and b id is found in our master sampling sites
-                #print("Match found:", f_b_id)
                 continue
             else:
-                #print("No match found:", f_b_id)
                 if ("Method Blank" not in f_b_id):
                     if f_b_id[0:2] != "0_":
                         no_match.append(f_b_id)
@@ -81,21 +98,17 @@ def main(sample_outing_name, qaqc_path, sample_pts_path, raw_data_path, processe
     if len(no_match)>0:
         print('Add corrected IDs to the Fixed ID spreadsheet')
 
-    #### If an ID from F&B does not match the sampling sites id, replace the sample id here using this syntax
-    ### TODO: join and replace from fixed ID spreadsheet
-    # change sample ID DPS1 -> DPS-1
-    results_df['Sample ID'] = np.where(results_df['Sample ID'] == 'DPS1', 'DPS-1', results_df['Sample ID'])
+    # Replace mismatched IDs from F&B to the correct sampling ID
+    fixed_ids = pd.read_excel(fixed_id_path, sheet_name = "IDs")
 
-    # change sample ID SPB-0159-S-1 -> SPB-O159-S-1
-    results_df['Sample ID'] = np.where(results_df['Sample ID'] == 'SPB-0159-S-1', 'SPB-O159-S-1', results_df['Sample ID'])
+    results_df = results_df.merge(fixed_ids, how = 'left', left_on = 'Sample ID', right_on = 'FB_ID', indicator = True)
+    results_df['Sample ID'] = np.where(results_df['_merge']=='both', results_df['Fixed_ID'], results_df['Sample ID'])
+    results_df.drop(columns = ['_merge', 'Fixed_ID'])
 
-    #### QAQC check to make sure the dates line up
-    sample_dates = sample_pts_gdf['Date'].unique()
-    f_b_dates = results_df['Field Collection Start Date'].unique()
-
-    #### QAQC: if any dates are mismatched, replace below using the same syntax
-    # replace typo from 11/17 -> 11/07
-    results_df['Field Collection Start Date'] = np.where(results_df['Field Collection Start Date']=='2023-11-17 00:00:00', np.datetime64('2023-11-07'), results_df['Field Collection Start Date'])
+    fixed_dates = pd.read_excel(fixed_id_path, sheet_name = "Dates")
+    results_df = results_df.merge(fixed_dates, how = 'left', left_on = 'Field Collection Date', right_on = 'FB_Date', indicator = True)
+    results_df['Field Collection Date'] = np.where(results_df['_merge']=='both', results_df['Fixed_Date'], results_df['Field Collection Date'])
+    results_df.drop(columns = ['_merge', 'Fixed_Date'])
 
     # remove any rows that were not field data
     results_df =  results_df[results_df['Field Collection Start Date'].isna() == False]
@@ -115,13 +128,12 @@ def main(sample_outing_name, qaqc_path, sample_pts_path, raw_data_path, processe
     # clean up pcb values in order to make the join correctly with screening levels
     results_df['Result Parameter Name_clean'] = results_df['Result Parameter Name'].apply(lambda x: clean_pcb(x))
 
-    # replace Lube Oil to Diesel Range Organics
+    # replace Lube Oil to Motor Oil to match the screening level spreadsheet
     results_df['Result Parameter Name_clean'] = np.where(results_df['Result Parameter Name_clean'] == 'Lube Oil', 'Motor Oil', results_df['Result Parameter Name_clean'])
 
     # calculate total PCBs for epa1668
     tot_pcbs = results_df[results_df['Result Method'] == 'EPA1668C']
     tot_pcbs = tot_pcbs.groupby(by =['Sample ID', 'Field Collection Start Date', 'Sample Matrix', 'Sample Matrix_clean','Result Value Units']).agg({'Result Value': ['sum']}).reset_index()
-
     drop_levels(tot_pcbs)
     tot_pcbs['Result Parameter Name_clean'] = 'Total PCBs'
 
@@ -132,4 +144,5 @@ def main(sample_outing_name, qaqc_path, sample_pts_path, raw_data_path, processe
                             'Result Parameter Name','Result Parameter Name_clean','Result Value', 'Result Value Units', 'Result Reporting Limit', 
                             'Result Reporting Limit Type', 'Result Detection Limit','Result Detection Limit Type', 'Result Data Qualifier', 'Result Method']]
 
+    # Output results processed file
     results_df.to_csv(processed_path, index = False)